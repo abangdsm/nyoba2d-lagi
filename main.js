b2d9a29ebb6a153505bb52058fda8e049792ca42
abangdsm <id.reversal@gmail.com>
2023-01-31 13:52:41 +0700

    Menambahkan background

--- main.js
+++ main.js
@@ -12,10 +12,29 @@ let frame = 0;
 let score = 0;
 let gameSpeed = 2;
 
+const background = new Image();
+background.src = 'BG.png';
+const BG = {
+    x1: 0,
+    x2: canvas.width,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height
+}
+function handleBackground(){
+    if(BG.x1 <= -BG.width) BG.x1 = BG.width;
+    else BG.x1 -= gameSpeed;
+    if(BG.x2 <= -BG.width) BG.x2 = BG.width;
+    else BG.x2 -= gameSpeed;
+    ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height);
+    ctx.drawImage(background, BG.x2, BG.y, BG.width, BG.height);
+}
 
+// Jalankan Animasi
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // ctx.fillRect(10, canvas.height - 90, 50, 50);
+    handleBackground();
     handleObstacles();
     handleParticles();
     bird.update();
@@ -56,3 +75,4 @@ function handleCollisions(){
         }
     }
 }
+
